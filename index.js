82c6ab16fd0b7bab3dd51efc9c1fd6c8275e742c
Albert Sun <fatalsun@fb.com>
2018-05-07 10:10:05 -0700

    Import and then export instead of export ... from ...
    This is to work around some test failures for infrastructure that hasn't been updated to support ES8 re-export syntax yet.

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ import MapView, { Animated, MAP_TYPES, ProviderPropType } from './lib/components
 import Marker from './lib/components/MapMarker.js';
 import Overlay from './lib/components/MapOverlay.js';
 
-export { default as Polyline } from './lib/components/MapPolyline.js';
-export { default as Polygon } from './lib/components/MapPolygon.js';
-export { default as Circle } from './lib/components/MapCircle.js';
-export { default as UrlTile } from './lib/components/MapUrlTile.js';
-export { default as LocalTile } from './lib/components/MapLocalTile.js';
-export { default as Callout } from './lib/components/MapCallout.js';
-export { default as AnimatedRegion } from './lib/components/AnimatedRegion.js';
+import { default as Polyline } from './lib/components/MapPolyline.js';
+import { default as Polygon } from './lib/components/MapPolygon.js';
+import { default as Circle } from './lib/components/MapCircle.js';
+import { default as UrlTile } from './lib/components/MapUrlTile.js';
+import { default as LocalTile } from './lib/components/MapLocalTile.js';
+import { default as Callout } from './lib/components/MapCallout.js';
+import { default as AnimatedRegion } from './lib/components/AnimatedRegion.js';
 
-export { Marker, Overlay };
+export { Marker, Overlay, Polyline, Polygon, Circle, UrlTile, LocalTile, Callout, AnimatedRegion };
 export { Animated, MAP_TYPES, ProviderPropType };
 
 export const PROVIDER_GOOGLE = MapView.PROVIDER_GOOGLE;
